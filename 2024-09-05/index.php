c4c80eca064437d004693c71854401d40d54d9b9
Rizky251189 <rizkyhadistya72@gmail.com>
2024-09-12 09:12:26 +0700

    Add files via upload

---
+++ 2024-09-05/index.php
@@ -0,0 +1,70 @@
+<?php
+for ($i=0; $i < =10 ; $i++) { 
+    echo $i;
+    # code...
+}
+echo "<br>";
+for ($i=0; $i < ; $i++) { 
+    # code...
+}
+echo "<br>";
+$ganjil=5;
+echo $ganjil;
+
+echo "<br>";
+for ($i=0; $i < ; $i++) { 
+
+    $ganjil=$i % 2;
+    //echo $ganjil;
+    echo $ganjil;
+    if ($ganjil==1) {
+        echo $ganjil;
+        # code...
+    }
+    # code...
+}
+echo "<br>";
+$kkm = 80;
+$nilai = 75;
+if ($nilai > $kkm ) {
+    echo "lulus";
+    # code...
+}
+else {
+    echo "tidak lulus;"
+}
+echo "<br>";
+$status = "tidak lulus"
+if ($nilai > $kkm ) {
+    $status "lulus";
+    # code...
+}
+echo $status
+
+echo "<br>";
+
+$raport = 0;
+$tugaas = 0;
+
+if ($tugas == 1) {
+    $raport = 80;
+    # code...
+}
+echo $raport;
+
+echo "<br>"
+$bulan = 100;
+$tanggal = 0;
+$keterangan = "salah";
+
+if ($bulan > 0 && $bulan <13) {
+    $keterangan = "benar";
+    # code...
+}
+echo $keterangan;
+
+
+
+
+
+?>
